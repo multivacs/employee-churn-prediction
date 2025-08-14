8bce59083e09d187856921a73795233757903d38
Mario <41269835+multivacs@users.noreply.github.com>
2025-08-14 12:54:11 +0200

    feat: add id column parameter in load_data function

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from src.config import RAW_EMPLOYEE_CHURN_FILE
 
 
-def load_data(path: Path = RAW_EMPLOYEE_CHURN_FILE):
-    return pd.read_csv(path, sep=',', decimal='.', index_col= 'Employee ID')
+def load_data(path: Path = RAW_EMPLOYEE_CHURN_FILE, id_column: str = 'id'):
+    return pd.read_csv(path, sep=',', decimal='.', index_col = id_column)
