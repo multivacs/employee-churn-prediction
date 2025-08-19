c963f38a7ec62be98a028b39facc9e43ab7f78ec
Mario <mcp105@alu.ua.es>
2025-08-19 23:08:07 +0200

    feat: add na value format to load dataset

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from src.config import RAW_EMPLOYEE_CHURN_FILE
 
 
-def load_data(path: Path = RAW_EMPLOYEE_CHURN_FILE, id_column: str = 'id'):
-    return pd.read_csv(path, sep=',', decimal='.', index_col = id_column)
+def load_data(path: Path = RAW_EMPLOYEE_CHURN_FILE, id_column: str = 'id', na_values: str = '#N/D'):
+    return pd.read_csv(path, sep=',', decimal='.', index_col = id_column, na_values = na_values)
