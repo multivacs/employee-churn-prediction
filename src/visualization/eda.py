3cfaebd80c9c90a8d99f604fffbea21ced04af9c
Mario <41269835+multivacs@users.noreply.github.com>
2025-09-04 13:24:28 +0200

    feat(eda): add function to calculate economic impact of employee turnover

--- src/visualization/eda.py
+++ src/visualization/eda.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from math import ceil
 import matplotlib.pyplot as plt
+import numpy as np
 
 def visualize_categorical(categoricals: pd.Series):
     """
@@ -67,3 +68,27 @@ def analysis_by_target(df: pd.DataFrame, target: str, feature: str, normalize = 
     plt.show()
 
     return analysis
+
+
+def feature_engineering_economic_costs(df: pd.DataFrame):
+    """
+    Create new features related to employee turnover costs.
+    """
+
+    # Create annual salary feature
+    df['annual_salary'] = df['monthly_salary'] * 12
+
+    # Define economic impact constants
+    conditions = [(df['annual_salary'] < 30000),
+                  (df['annual_salary'] >= 30000) & (df['annual_salary'] < 50000),
+                  (df['annual_salary'] >= 50000) & (df['annual_salary'] < 75000),
+                  (df['annual_salary'] >= 75000)]
+    
+    costs = [df.annual_salary * 0.161, # 16.1% for <30k
+             df.annual_salary * 0.197, # 19.7% for 30k-50k
+             df.annual_salary * 0.204, # 20.4% for 50k-75k
+             df.annual_salary * 0.21] # 21% for >=75k
+    
+    df['cost_of_turnover'] = np.select(conditions, costs, default=0)
+
+    return df
