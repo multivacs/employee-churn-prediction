12cf5e04fb22b279c3214a2747cfaffecefbce00
Mario <41269835+multivacs@users.noreply.github.com>
2025-09-19 00:48:03 +0200

    bug: add parent folder to path before importing src.config

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,7 +1,14 @@
 import pandas as pd
 from pathlib import Path
-from src.config import RAW_EMPLOYEE_CHURN_FILE
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from config import RAW_EMPLOYEE_CHURN_FILE
 
 
 def load_data(path: Path = RAW_EMPLOYEE_CHURN_FILE, id_column: str = 'id', na_values: str = '#N/D'):
-    return pd.read_csv(path, sep=',', decimal='.', index_col = id_column, na_values = na_values)
+    return pd.read_csv(path, sep=',', decimal='.', index_col=id_column, na_values=na_values)
