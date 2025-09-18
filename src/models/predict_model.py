3b05101deb834e83156881e3a6e4b69e2bea6481
Mario <41269835+multivacs@users.noreply.github.com>
2025-09-19 00:46:46 +0200

    feat(model): update predict_model function to return both predictions and probabilities

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,5 +5,6 @@ def predict_model(model, X: pd.DataFrame):
     Generate predictions using the trained model.
     """
 
-    predictions = model.predict_proba(X)[:, 1]  # Probability of the positive class
-    return predictions
+    preds = model.predict(X) 
+    pred_prob = model.predict_proba(X)[:, 1]  # Probability of the positive class
+    return preds, pred_prob
